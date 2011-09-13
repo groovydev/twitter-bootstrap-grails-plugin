73d1d2cd7cbbc657fd180f86d9832e8ae77a5c93
Karol Balejko <info@groovydev.org>
2011-09-13 14:32:01 +0200

    added less support prod environment use minified css

--- grails-app/conf/BootstrapResources.groovy
+++ grails-app/conf/BootstrapResources.groovy
@@ -1,7 +1,32 @@
+def dev = grails.util.GrailsUtil.isDevelopmentEnv()
+
+def applicationContext = org.codehaus.groovy.grails.commons.ApplicationHolder.application.mainContext
+def lesscssPlugin = applicationContext.pluginManager.getGrailsPlugin('lesscss-resources')
+
+def cssVersion = '1.2.0' 
+def cssFile = "bootstrap-${cssVersion}.css"
+def cssminFile = "bootstrap-${cssVersion}.min.css"
+def lesscssFile = "bootstrap.less"
+
+def file
+def attrs
+
+if (lesscssPlugin != null) {
+    file = lesscssFile
+    attrs = [rel: "stylesheet/less", type:'css']
+} else {
+    file = (dev ? cssFile : cssminFile)
+    attrs = [:]
+}
+
 modules = {
     
     bootstrap {
-        resource url:[plugin: 'twitter-bootstrap', dir: 'css', file:'bootstrap-1.2.0.css'], disposition: 'head'
+        resource url:[plugin: 'twitter-bootstrap', dir: 'css', file: file], attrs:attrs, disposition: 'head', exclude:'minify', bundle: 'bundle_bootstrap'
+    }
+
+    'bootstrap-less' {
+        resource url:[plugin: 'twitter-bootstrap', dir: 'css', file: lesscssFile], attrs:[rel: "stylesheet/less", type:'css'], disposition: 'head', exclude:'minify', bundle: 'bundle_bootstrap'
     }
 
 }
