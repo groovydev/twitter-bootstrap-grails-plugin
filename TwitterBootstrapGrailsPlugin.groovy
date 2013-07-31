bda266349f1b6b8c7c9b950c3fd3e142b9bb3649
David Estes <davydotcom@gmail.com>
2013-07-31 13:11:10 -0400

    adding asset-pipeline support

--- TwitterBootstrapGrailsPlugin.groovy
+++ TwitterBootstrapGrailsPlugin.groovy
@@ -1,13 +1,13 @@
 import org.groovydev.TwitterBootstrapTagLib
 
 class TwitterBootstrapGrailsPlugin {
-    
+
     // the plugin version
     def version = "2.3.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['resources':'1.0 > *']
+    // def dependsOn = ['resources':'1.0 > *']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
