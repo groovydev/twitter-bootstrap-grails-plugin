bc6085140744f3b6c6ef9342a7329b9be758164c
Michael Legart <ml@code3.dk>
2013-08-15 20:02:35 +0200

    Did some spring cleanup and removed unused files

--- TwitterBootstrapGrailsPlugin.groovy
+++ TwitterBootstrapGrailsPlugin.groovy
@@ -1,17 +1,6 @@
-import org.groovydev.TwitterBootstrapTagLib
-
 class TwitterBootstrapGrailsPlugin {
-
-    // the plugin version
     def version = "3.0.0.RC2"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
-
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
-
     def author = "Karol Balejko"
     def authorEmail = "kb@groovydev.com"
     def developers = [ [ name: "Nicholas Wittstruck", email: "nicholas@spotmapping.com" ]]
@@ -23,10 +12,8 @@ Bootstrap is Twitter's toolkit for kickstarting CSS for websites, apps, and more
 To get started with Bootstrap checkout the [Bootstrap Documentation|http://getbootstrap.com/getting-started/]!
 """
 
-    // URL to the plugin's documentation
     def documentation = "https://github.com/groovydev/twitter-bootstrap-grails-plugin/blob/master/README.md"
     def license = "APACHE"
     def issueManagement = [ system: "github", url: "https://github.com/groovydev/twitter-bootstrap-grails-plugin/issues" ]
     def scm = [ url: "https://github.com/groovydev/twitter-bootstrap-grails-plugin" ]
-
 }
