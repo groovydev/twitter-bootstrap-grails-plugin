b4fae2d237e78a565010c22a95fc44f44500f65b
Michael Legart <ml@code3.dk>
2013-08-15 20:02:35 +0200

    Backport from master
    Did some spring cleanup and removed unused files
    
    Conflicts:
    	TwitterBootstrapGrailsPlugin.groovy
    	grails-app/conf/BuildConfig.groovy
    	plugin.xml

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,24 +1,17 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.work.dir = 'target'
+
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
-        mavenCentral()
-    }
-    plugins {
-        build(":release:2.2.1") { export = false }
-        runtime(":resources:1.1.6") { export = false }
-    }
-    dependencies {
-    }
+	inherits 'global'
+	log 'warn'
+
+	repositories {
+		grailsCentral()
+	}
+
+	plugins {
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+			export = false
+		}
+		runtime(":resources:1.2") { export = false }
+	}
 }
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,13 +1,4 @@
-// configuration for plugin testing - will not be included in the plugin zip
- 
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
