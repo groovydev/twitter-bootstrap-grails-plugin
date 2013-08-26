8f6513d7549736861a060a7d79693e0d4f798a5d
Chris Rebert <chris.rebert@hulu.com>
2013-08-26 14:31:43 -0700

    reflect Bootstrap's org change

--- TwitterBootstrapGrailsPlugin.groovy
+++ TwitterBootstrapGrailsPlugin.groovy
@@ -5,10 +5,10 @@ class TwitterBootstrapGrailsPlugin {
     def authorEmail = "kb@groovydev.com"
     def developers = [ [ name: "Nicholas Wittstruck", email: "nicholas@spotmapping.com" ]]
     def organization = [ name: "GroovyDev", url: "http://groovydev.com/" ]
-    def title = "Plugin Twitter Bootstrap CSS framework resource files"
-    def description = """Provides Twitter Bootstrap CSS framework resource files.
+    def title = "Plugin Bootstrap CSS framework resource files"
+    def description = """Provides Bootstrap CSS framework resource files.
 
-Bootstrap is Twitter's toolkit for kickstarting CSS for websites, apps, and more. It includes base CSS styles for typography, forms, buttons, tables, grids, navigation, alerts, and more.
+Bootstrap is a toolkit for kickstarting CSS for websites, apps, and more. It includes base CSS styles for typography, forms, buttons, tables, grids, navigation, alerts, and more.
 To get started with Bootstrap checkout the [Bootstrap Documentation|http://getbootstrap.com/getting-started/]!
 """
 
