9bfabac7a75c3d938fd43d19ab8ee527ea2714a2
Michael Legart <ml@code3.dk>
2013-11-07 09:36:57 +0100

    Upgrade to Bootstrap 3.0.2

--- TwitterBootstrapGrailsPlugin.groovy
+++ TwitterBootstrapGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class TwitterBootstrapGrailsPlugin {
-    def version = "3.0.1"
+    def version = "3.0.2"
     def grailsVersion = "1.3.7 > *"
     def author = "Karol Balejko"
     def authorEmail = "kb@groovydev.com"
