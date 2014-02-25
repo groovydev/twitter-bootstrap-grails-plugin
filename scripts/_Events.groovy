974bd4dea66940d3ef7de51a29b82055c250c035
David Estes <davydotcom@gmail.com>
2014-02-25 13:03:52 -0800

    Removed deprecated call to ApplicationHolder which breaks grails 2.4

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,8 +1,5 @@
-import org.codehaus.groovy.grails.commons.ApplicationHolder
-
 eventAssetPrecompileStart = { assetConfig ->
-	def grailsApplication = ApplicationHolder.getApplication()
-	if(!grailsApplication.config.grails.assets.plugin."twitter-bootstrap".excludes || grailsApplication.config.grails.assets.plugin."twitter-bootstrap".excludes.size() == 0) {
-		grailsApplication.config.grails.assets.plugin."twitter-bootstrap".excludes = ["**/*.less"]
+	if(!config.grails.assets.plugin."twitter-bootstrap".excludes || config.grails.assets.plugin."twitter-bootstrap".excludes.size() == 0) {
+		config.grails.assets.plugin."twitter-bootstrap".excludes = ["**/*.less"]
 	}
 }
