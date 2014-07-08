b305622b6b33283b131d9988a7f8f6f4fe1fb481
Michael Legart <ml@code3.dk>
2014-07-08 21:14:07 +0200

    Fixed pagination without 'fixtaglib' for grails 2.4
    g:paginate was moved from RenderTaglib to UrlMappingTagLib in 2.4:
    https://github.com/grails/grails-core/commit/7a031f2112918dc1ef4241c7b3d9ff9ce6ae85ca
    
    fixes #113

--- TwitterBootstrapGrailsPlugin.groovy
+++ TwitterBootstrapGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class TwitterBootstrapGrailsPlugin {
-    def version = "3.2.0"
+    def version = "3.2.1"
     def grailsVersion = "1.3.7 > *"
     def author = "Karol Balejko"
     def authorEmail = "kb@groovydev.com"
