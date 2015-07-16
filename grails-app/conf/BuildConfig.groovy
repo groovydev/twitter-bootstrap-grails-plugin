5954fcf52a473658b560aef9a2dd2014a7b6e9c4
Tom Crossland <tom.crossland@bluetab.net>
2015-07-16 12:28:05 +0200

    Update plugin dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,9 +11,9 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build ':release:3.0.1', ':rest-client-builder:1.0.3', {
+		build ':release:3.1.1', ':rest-client-builder:2.1.1', {
 			export = false
 		}
-		runtime(":resources:1.2.8") { export = false }
+		runtime(":resources:1.2.14") { export = false }
 	}
 }
